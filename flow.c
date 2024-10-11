82cc07d33d82f65da4f3eecf6423439d5b92b513
Pratham Gupta <pratham.gupta2k@gmail.com>
2024-10-11 11:06:04 -0400

    Made basic functionality work

--- flow.c
+++ flow.c
@@ -1,52 +1,118 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <fcntl.h>
+#include <unistd.h> 
 
-#define MAX_NODES 10
+#define TABLE_SIZE 10
 #define BUFFER_SIZE 1024
 
-// Structure for holding node and pipe information
-typedef struct {
+typedef struct Node {
     char *name;
     char *command;
+    struct Node *next;
 } Node;
 
-typedef struct {
-    char *from;
+typedef struct Pipe{
+    char *name;
+    char *from; 
     char *to;
+    struct Pipe *next;
 } Pipe;
 
-Node nodes[MAX_NODES];
-Pipe pipes[MAX_NODES];
-int node_count = 0;
-int pipe_count = 0;
 
-// Function to parse .flow file
+Node *node_table[TABLE_SIZE];
+Pipe *pipe_table[TABLE_SIZE];
+
+// Simple hash function
+unsigned int hash(const char *key) {
+    unsigned int hash = 0;
+    while (*key) {
+        hash = (hash << 5) + *key++;
+    }
+    return hash % TABLE_SIZE;
+}
+
+// Add a node to the hashmap
+void add_node(const char *name, const char *command) {
+    unsigned int index = hash(name);
+    Node *new_node = (Node *)malloc(sizeof(Node));
+    new_node->name = strdup(name);
+    new_node->command = strdup(command);
+    new_node->next = node_table[index];
+    node_table[index] = new_node;
+}
+
+// Add a pipe to the hashmap
+void add_pipe(const char *name, const char *from, const char *to) {
+    unsigned int index = hash(name); 
+    Pipe *new_pipe = (Pipe *)malloc(sizeof(Pipe));
+    new_pipe->name = strdup(name);
+    new_pipe->from = strdup(from);
+    new_pipe->to = strdup(to);
+    new_pipe->next = pipe_table[index];
+    pipe_table[index] = new_pipe;
+
+    printf("Added pipe: name=%s, from=%s, to=%s\n", name, from, to);
+}
+
+
+
+// Lookup a node in the hashmap
+Node *find_node(const char *name) {
+    unsigned int index = hash(name);
+    Node *node = node_table[index];
+    while (node) {
+        if (strcmp(node->name, name) == 0) {
+            return node;
+        }
+        node = node->next;
+    }
+    return NULL;
+}
+
+// Lookup a pipe in the hashmap
+Pipe *find_pipe(const char *name) {
+    unsigned int index = hash(name); 
+    Pipe *pipe = pipe_table[index];
+    while (pipe) {
+        if (strcmp(pipe->name, name) == 0) {
+            return pipe;
+        }
+        pipe = pipe->next;
+    }
+    return NULL; 
+}
+
+
+// Function to parse the .flow file and populate the hashmaps
 void parse_flow_file(const char *filename) {
     FILE *file = fopen(filename, "r");
     if (!file) {
         perror("Failed to open file");
         exit(EXIT_FAILURE);
     }
+
     char line[BUFFER_SIZE];
-    
     while (fgets(line, sizeof(line), file)) {
         if (strncmp(line, "node=", 5) == 0) {
-            nodes[node_count].name = strdup(strtok(line + 5, "\n"));
+            char *name = strdup(strtok(line + 5, "\n"));
             fgets(line, sizeof(line), file);
-            nodes[node_count].command = strdup(strtok(line + 8, "\n")); // skip 'command='
-            node_count++;
-        } else if (strncmp(line, "pipe=", 5) == 0) {
-            pipes[pipe_count].from = strdup(strtok(NULL, "\n"));
+            char *command = strdup(strtok(line + 8, "\n"));
+            add_node(name, command);
+        } 
+        else if (strncmp(line, "pipe=", 5) == 0) {
+            char *pipe_name = strdup(strtok(line + 5, "\n")); 
+            fgets(line, sizeof(line), file); 
+            char *from = strdup(strtok(line + 5, "\n"));
             fgets(line, sizeof(line), file);
-            pipes[pipe_count].to = strdup(strtok(NULL, "\n"));
-            pipe_count++;
+            char *to = strdup(strtok(line + 3, "\n"));
+            add_pipe(pipe_name, from, to);
         }
     }
     fclose(file);
 }
+
+
 
 // Function to run a single command
 void run_command(char *command) {
@@ -71,10 +137,9 @@ void run_pipe(char *from_command, char *to_command) {
     }
 
     if (pid1 == 0) {
-        // Child 1: runs `from_command`
-        close(fd[0]); // Close read end
-        dup2(fd[1], STDOUT_FILENO); // Redirect stdout to pipe
-        close(fd[1]); // Close write end
+        close(fd[0]);
+        dup2(fd[1], STDOUT_FILENO);
+        close(fd[1]);
         run_command(from_command);
     }
 
@@ -85,14 +150,12 @@ void run_pipe(char *from_command, char *to_command) {
     }
 
     if (pid2 == 0) {
-        // Child 2: runs `to_command`
-        close(fd[1]); // Close write end
-        dup2(fd[0], STDIN_FILENO); // Redirect stdin to pipe
-        close(fd[0]); // Close read end
+        close(fd[1]);
+        dup2(fd[0], STDIN_FILENO); 
+        close(fd[0]);
         run_command(to_command);
     }
 
-    // Parent process: close pipes and wait for children to finish
     close(fd[0]);
     close(fd[1]);
     wait(NULL);
@@ -110,21 +173,24 @@ int main(int argc, char *argv[]) {
 
     parse_flow_file(flow_file);
 
-    for (int i = 0; i < pipe_count; i++) {
-        if (strcmp(pipes[i].from, action) == 0) {
-            for (int j = 0; j < node_count; j++) {
-                if (strcmp(nodes[j].name, pipes[i].from) == 0) {
-                    for (int k = 0; k < node_count; k++) {
-                        if (strcmp(nodes[k].name, pipes[i].to) == 0) {
-                            run_pipe(nodes[j].command, nodes[k].command);
-                            return 0;
-                        }
-                    }
-                }
-            }
+    Pipe *pipe = find_pipe(action);
+    if (pipe) {
+        printf("Found pipe: name=%s, from=%s, to=%s\n", pipe->name, pipe->from, pipe->to); 
+
+        Node *from_node = find_node(pipe->from);
+        Node *to_node = find_node(pipe->to);
+
+        if (from_node && to_node) {
+            printf("Running pipe: %s | %s\n", from_node->command, to_node->command);
+            run_pipe(from_node->command, to_node->command);
+        } else {
+            fprintf(stderr, "Invalid nodes for the pipe\n");
+            return EXIT_FAILURE;
         }
+    } else {
+        fprintf(stderr, "No matching action found in the flow file.\n");
+        return EXIT_FAILURE;
     }
 
-    fprintf(stderr, "No matching action found in the flow file.\n");
-    return EXIT_FAILURE;
+    return 0;
 }
